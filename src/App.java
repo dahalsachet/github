1532c1ccd27132ab7fb0ea8d44b0e85dff5e5fef
Your Name <179803901+dahalsachet@users.noreply.github.com>
2024-11-05 14:29:39 +0545

    again i change 4 as 44

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int age= 4;
+        int age= 44;
         if(age>0 && age<24){
             System.out.println(age +". You are child.");
          } else{
