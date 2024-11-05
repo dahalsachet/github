b29b84c6dc2c1e7431a38bbfabc113650b39f27b
Your Name <179803901+dahalsachet@users.noreply.github.com>
2024-11-05 14:27:54 +0545

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int age= 24;
+        if(age>0 && age<24){
+            System.out.println(age +". You are child.");
+         } else{
+            System.out.println(age +". You are adult.");
+         }
+         System.out.println("Press space to exit");
+        }
+}
